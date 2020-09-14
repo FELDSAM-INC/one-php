e5d29058a5a8376d46dddb14c263fa9d25c8896e
Kristian Feldsam <feldsam@gmail.com>
2020-09-14 22:23:21 +0200

    Implemented basic functionality for client
    Looks like XSD schemas are incomplete, so we need to crete few classes manually with dynamic parameters...
    
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

--- index.php
+++ index.php
@@ -1,35 +1,9 @@
 <?php
 
+use OnePhp\One;
+
 require __DIR__ . '/vendor/autoload.php';
 
-
-use PhpXmlRpc\Client;
-use PhpXmlRpc\Request;
-use PhpXmlRpc\Value;
-
-// prepare serializer
-
-
 // do request
-$client = new Client('http://192.168.44.23:2633/RPC2');
-$response = $client->send(new Request('one.userpool.info', array(new Value('authstring'))));
-
-$success = (bool) $response->value()[0]->scalarval();
-$result = (string) $response->value()[1]->scalarval();
-
-if($success !== true) {
-    die('Error: '.$result);
-}
-
-$result = str_replace('<USER_POOL>', "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<USER_POOL xmlns=\"http://opennebula.org/XMLSchema\">", $result);
-
-//echo $result;
-
-// deserialize the XML into object
-$object = $serializer->deserialize($result, 'One\UserPool', 'xml');
-
-foreach ($object->getUser() as $user) {
-    if ($user->getId() == 2) {
-        print_r($user);
-    }
-}
+$client = new One('http://192.168.44.23:2633/RPC2', 'authstring');
+$response = $client->vmInfo(2040);
--- src/OnePhp/One.php
+++ src/OnePhp/One.php
@@ -7,6 +7,9 @@ use JMS\Serializer\SerializerBuilder;
 use JMS\Serializer\Handler\HandlerRegistryInterface;
 use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\BaseTypesHandler;
 use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\XmlSchemaDateHandler;
+use PhpXmlRpc\Client;
+use PhpXmlRpc\Request;
+use PhpXmlRpc\Value;
 
 class One {
 
@@ -15,17 +18,108 @@ class One {
      */
     private $serializer;
 
-    private function prepareSerializer() {
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var string
+     */
+    private $authString;
+
+    private static $xmlrpcTypes = [
+        "boolean" => "boolean",
+        "integer" => "int",
+        "double" => "double",
+        "string" => "string",
+        "array" => "array",
+        "object" => "struct",
+        "NULL" => "null",
+    ];
+
+    public function __construct($url, $authString) {
+        $this->client = new Client($url);
+        $this->authString = $authString;
+
+        $this->prepareSerializer();
+    }
+
+    /**
+     * Prepare serializer object
+     */
+    private function prepareSerializer(): void {
         $serializerBuilder = SerializerBuilder::create();
-        $serializerBuilder->addMetadataDir('./src/OnePhp/metadata', 'One');
+        $serializerBuilder->addMetadataDir(__DIR__.'/metadata', 'One');
 
         $serializerBuilder->configureHandlers(function (HandlerRegistryInterface $handler) use ($serializerBuilder) {
             $serializerBuilder->addDefaultHandlers();
             $handler->registerSubscribingHandler(new BaseTypesHandler()); // XMLSchema List handling
             $handler->registerSubscribingHandler(new XmlSchemaDateHandler()); // XMLSchema date handling
-            $handler->registerSubscribingHandler(new \OnePhp\AnyTypeHandler());
+            $handler->registerSubscribingHandler(new AnyTypeHandler());
         });
 
         $this->serializer = $serializerBuilder->build();
+    }
+
+    public function __call($name, $args) {
+        $method = 'one.'.$this->snakeCaseToDot($name);
+        $params = [new Value($this->authString)];
+
+        foreach ($args as $arg) {
+            $type = gettype($arg);
+            $params[] = new Value($arg, self::$xmlrpcTypes[$type]);
+        }
+
+        $response = $this->client->send(new Request($method, $params));
+
+        $success = (bool) $response->value()[0]->scalarval();
+        $result = (string) $response->value()[1]->scalarval();
+        $code = (string) $response->value()[2]->scalarval();
+
+        if($success !== true) {
+            throw new \RuntimeException(sprintf(
+                'XML-RPC API Call ends with error "%s" and code "%s"',
+                $result,
+                $code
+            ));
+        }
+
+        // look for root element
+        preg_match('/^<([^>]+)>/', $result, $root);
+
+        // add XML header to result
+        $result = '<?xml version="1.0" encoding="UTF-8"?>'."\n".$result;
+        $result = str_replace($root[0], '<'.$root[1].' xmlns="http://opennebula.org/XMLSchema">', $result);
+
+        // deserialize the XML into object
+        return $this->serializer->deserialize($result, 'One\\'.$this->camelCase($root[1]), 'xml');
+    }
+
+    /**
+     * Converts 'ModelName' to 'model.name'.
+     * @param string $word
+     * @return string
+     */
+    private function snakeCaseToDot(string $word) : string
+    {
+        $tableized = preg_replace('~(?<=\\w)([A-Z])~u', '.$1', $word);
+
+        if ($tableized === null) {
+            throw new \RuntimeException(sprintf(
+                'preg_replace returned null for value "%s"',
+                $word
+            ));
+        }
+
+        return mb_strtolower($tableized);
+    }
+
+    /**
+     * 'model_name' to 'ModelName'.
+     */
+    public function camelCase(string $word) : string
+    {
+        return str_replace([' ', '_', '-'], '', ucwords(mb_strtolower($word), ' _-'));
     }
 }
