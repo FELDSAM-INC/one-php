c5a1d255ec50ea606b75264d110f15672efb399c
Kristian Feldsam <feldsam@gmail.com>
2020-09-20 14:18:08 +0200

    Added overided class
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

---
+++ src/Gregwar/RST/HTML/Nodes/TableNode.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Gregwar\RST\HTML\Nodes;
+
+use Gregwar\RST\Nodes\TableNode as Base;
+
+class TableNode extends Base
+{
+    public function render()
+    {
+        $html = '<table>';
+        foreach ($this->data as $k=>&$row) {
+            if (!$row) {
+                continue;
+            }
+
+            $html .= '<tr>';
+            foreach ($row as &$col) {
+                $html .= isset($this->headers[$k]) ? '<th>' : '<td>';
+                $html .= $col->render();
+                $html .= isset($this->headers[$k]) ? '</th>' : '</td>';
+            }
+            $html .= '</tr>';
+        }
+        $html .= '</table>';
+
+        return $html;
+    }
+
+    public function renderArray() {
+        $table = [];
+
+        foreach ($this->data as $k=>&$row) {
+            if (!$row) {
+                continue;
+            }
+
+            $tableRow = [];
+            foreach ($row as &$col) {
+                $tableRow[] = $col->render();
+            }
+
+            $table[] = $tableRow;
+        }
+
+        return $table;
+    }
+}
